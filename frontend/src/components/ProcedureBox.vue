91e60a0043044ef014b9a53e85c988530c6659db
임가현 <dlga7723@gmail.com>
2023-11-07 20:53:43 +0900

    Fix: NavBar 마우스 효과 및 반응형 구현

--- frontend/src/components/ProcedureBox.vue
+++ frontend/src/components/ProcedureBox.vue
@@ -25,12 +25,19 @@ export default { name: "procedureBox" };
 }
 .procedure {
   display: flex;
-  margin: 30px;
-  border: 2px solid#000;
-  height: 50px;
-  width: 200px;
-  font-style: bold;
+  margin: 2vw; /* 반응형 마진 사용 (화면 너비의 2% 크기로 조절) */
+  border: 2px solid #000;
+  min-height: 50px; /* 최소 높이 설정 */
+  width: 40%; /* 너비를 화면 너비의 40%로 설정 */
+  font-weight: bold; /* 올바른 속성 이름 사용 (font-style 대신 font-weight) */
   justify-content: center;
   align-items: center;
+}
+
+/* 화면 너비가 768px 미만일 때 스타일 변경 */
+@media (max-width: 768px) {
+  .procedure {
+    width: 80%; /* 화면 너비의 80%로 변경 */
+  }
 }
 </style>
