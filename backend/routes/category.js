ddde804f5d1d49f87859133386b9773be0da0040
lee ga gyeong <rkrud5012@naver.com>
2023-11-12 21:44:32 +0900

    Feat: DB back 연결 로직 추가

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getAllNewsCategories } = require('../models/newsModel');
 
-// 여기서 '/api/news' 경로에 대한 요청을 처리할 라우트를 정의합니다.
+
 router.get('/', async (req, res) => {
   try {
     const categories = await getAllNewsCategories();
