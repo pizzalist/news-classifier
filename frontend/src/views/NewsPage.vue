3ebd10cfc7cf7423b48b970e5595932acee6f2eb
김민근 <mingeun@gimmingeun-ui-MacBookPro.local>
2023-11-12 14:34:04 +0900

    feat:프론트 백엔드 연결 준비

--- frontend/src/views/NewsPage.vue
+++ frontend/src/views/NewsPage.vue
@@ -9,10 +9,12 @@
           width="24"
           height="25"
           viewBox="0 0 24 25"
-          fill="none">
+          fill="none"
+        >
           <path
             d="M15.586 11.1569L11.636 7.20692C11.4538 7.01832 11.353 6.76571 11.3553 6.50352C11.3576 6.24132 11.4628 5.99051 11.6482 5.8051C11.8336 5.61969 12.0844 5.51452 12.3466 5.51224C12.6088 5.50997 12.8614 5.61076 13.05 5.79292L18.707 11.4499C18.8002 11.5426 18.8741 11.6527 18.9246 11.7741C18.9751 11.8954 19.001 12.0255 19.001 12.1569C19.001 12.2883 18.9751 12.4184 18.9246 12.5398C18.8741 12.6611 18.8002 12.7713 18.707 12.8639L13.05 18.5209C12.9578 18.6164 12.8474 18.6926 12.7254 18.745C12.6034 18.7974 12.4722 18.825 12.3394 18.8262C12.2066 18.8273 12.0749 18.802 11.952 18.7517C11.8291 18.7015 11.7175 18.6272 11.6236 18.5333C11.5297 18.4394 11.4555 18.3278 11.4052 18.2049C11.3549 18.082 11.3296 17.9503 11.3307 17.8175C11.3319 17.6847 11.3595 17.5535 11.4119 17.4315C11.4643 17.3095 11.5405 17.1992 11.636 17.1069L15.586 13.1569H6C5.73478 13.1569 5.48043 13.0516 5.29289 12.864C5.10536 12.6765 5 12.4221 5 12.1569C5 11.8917 5.10536 11.6373 5.29289 11.4498C5.48043 11.2623 5.73478 11.1569 6 11.1569H15.586Z"
-            fill="#697077" />
+            fill="#697077"
+          />
         </svg>
         <input type="date" />
       </div>
@@ -27,7 +29,8 @@
           :class="{
             selected: selectedIndustry === industry,
             unselected: selectedIndustry !== industry,
-          }">
+          }"
+        >
           {{ industry }}
         </span>
       </div>
@@ -35,16 +38,19 @@
         <div
           class="dropdown"
           v-if="selectedIndustry === '산업정책'"
-          id="FirstDropdown">
+          id="FirstDropdown"
+        >
           <div class="list">
             <div
               class="list-item"
               v-for="(item, index) in paginatedItems"
-              :key="index">
+              :key="index"
+            >
               <input
                 type="checkbox"
                 :id="'firstItem' + index"
-                v-model="item.checked" />
+                v-model="item.checked"
+              />
               <label :for="'firstItem' + index">
                 <div class="item-title">{{ item.title }}</div>
                 <div class="item-content">{{ item.content }}</div>
@@ -56,16 +62,19 @@
         <div
           class="dropdown"
           v-if="selectedIndustry === '건설/ESG'"
-          id="SecondDropdown">
+          id="SecondDropdown"
+        >
           <div class="list">
             <div
               class="list-item"
               v-for="(item, index) in paginatedItems"
-              :key="index">
+              :key="index"
+            >
               <input
                 type="checkbox"
                 :id="'secondItem' + index"
-                v-model="item.checked" />
+                v-model="item.checked"
+              />
               <label :for="'secondItem' + index">
                 <div class="item-title">{{ item.title }}</div>
                 <div class="item-content">{{ item.content }}</div>
@@ -77,16 +86,19 @@
         <div
           class="dropdown"
           v-if="selectedIndustry === '조선/ESG'"
-          id="ThirdDropdown">
+          id="ThirdDropdown"
+        >
           <div class="list">
             <div
               class="list-item"
               v-for="(item, index) in paginatedItems"
-              :key="index">
+              :key="index"
+            >
               <input
                 type="checkbox"
                 :id="'thirdItem' + index"
-                v-model="item.checked" />
+                v-model="item.checked"
+              />
               <label :for="'thirdItem' + index">
                 <div class="item-title">{{ item.title }}</div>
                 <div class="item-content">{{ item.content }}</div>
@@ -98,16 +110,19 @@
         <div
           class="dropdown"
           v-if="selectedIndustry === 'IT'"
-          id="FourthDropdown">
+          id="FourthDropdown"
+        >
           <div class="list">
             <div
               class="list-item"
               v-for="(item, index) in paginatedItems"
-              :key="index">
+              :key="index"
+            >
               <input
                 type="checkbox"
                 :id="'fourthItem' + index"
-                v-model="item.checked" />
+                v-model="item.checked"
+              />
               <label :for="'fourthItem' + index">
                 <div class="item-title">{{ item.title }}</div>
                 <div class="item-content">{{ item.content }}</div>
@@ -126,13 +141,15 @@
               width="30"
               height="30"
               viewBox="0 0 24 24"
-              fill="none">
+              fill="none"
+            >
               <path
                 d="M15 19L8 12L15 5"
                 stroke="#21272A"
                 stroke-width="2"
                 stroke-linecap="round"
-                stroke-linejoin="round" />
+                stroke-linejoin="round"
+              />
             </svg>
           </span>
           <span
@@ -142,25 +159,29 @@
               'page-number': true,
               'current-page': currentPage === page,
             }"
-            @click="gotoPage(page)">
+            @click="gotoPage(page)"
+          >
             {{ page }}
           </span>
           <span
             class="nextBtn"
             @click="nextPage"
-            :disabled="currentPage === totalPage">
+            :disabled="currentPage === totalPage"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="30"
               height="30"
               viewBox="0 0 24 24"
-              fill="none">
+              fill="none"
+            >
               <path
                 d="M9 5L16 12L9 19"
                 stroke="#21272A"
                 stroke-width="2"
                 stroke-linecap="round"
-                stroke-linejoin="round" />
+                stroke-linejoin="round"
+              />
             </svg>
           </span>
         </div>
@@ -178,7 +199,7 @@ export default {
     return {
       items: [
         [
-          { title: "산업정책", content: "Description for Item 1" },
+          { title: "산업정책test", content: "[test]Description for Item 1" },
           { title: "산업정책", content: "Description for Item 2" },
           { title: "산업정책", content: "Description for Item 3" },
           { title: "산업정책", content: "Description for Item 4" },
@@ -319,9 +340,20 @@ export default {
       if (confirmAddToCart) {
         // 예를 눌렀을 때 장바구니 페이지로 이동
         this.$router.push("/BasketPage");
+        const checkedItems = this.paginatedItems.filter((item) => item.checked);
+        checkedItems.forEach((item) => {
+          console.log("Selected Items title:",item.title, "Selected Items content:", item.content);
+          // 여기서 데이터베이스에 값을 보내는 로직을 추가하면 됩니다.
+        });
       } else {
         // 아니요를 눌렀을 때 추가만 하고 페이지 이동 없음
         // 실제로는 여기에 장바구니에 아이템을 추가하는 로직을 추가해야 합니다.
+        const checkedItems = this.paginatedItems.filter((item) => item.checked);
+        checkedItems.forEach((item) => {
+          console.log("Selected Items title:",item.title, "Selected Items content:", item.content);
+          // 여기서 데이터베이스에 값을 보내는 로직을 추가하면 됩니다.
+        });
+        // 추가적인 로직을 여기에 추가할 수 있음
       }
     },
   },
