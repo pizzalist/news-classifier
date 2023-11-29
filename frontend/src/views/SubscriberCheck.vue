097157870f7fb76ec73a8f00e59473022e5b1c1d
임가현 <dlga7723@gmail.com>
2023-11-29 23:45:08 +0900

    feat: 전송 화면

--- frontend/src/views/SubscriberCheck.vue
+++ frontend/src/views/SubscriberCheck.vue
@@ -1,47 +1,7 @@
 <template>
   <div>
     <ProcedureBox />
-
-    <div class="List">
-      <table>
-        <tr>
-          <th><input type="checkbox" /></th>
-          <th>이메일 주소</th>
-          <th>구독일</th>
-          <th>기타</th>
-        </tr>
-        <tr>
-          <td><input type="checkbox" /></td>
-          <td>example@email.com</td>
-          <td>2023-11-07</td>
-          <td>기타 정보</td>
-        </tr>
-        <tr>
-          <td><input type="checkbox" /></td>
-          <td>example@email.com</td>
-          <td>2023-11-07</td>
-          <td>기타 정보</td>
-        </tr>
-        <tr>
-          <td><input type="checkbox" /></td>
-          <td>example@email.com</td>
-          <td>2023-11-07</td>
-          <td>기타 정보</td>
-        </tr>
-        <tr>
-          <td><input type="checkbox" /></td>
-          <td>example@email.com</td>
-          <td>2023-11-07</td>
-          <td>기타 정보</td>
-        </tr>
-        <tr>
-          <td><input type="checkbox" /></td>
-          <td>example@email.com</td>
-          <td>2023-11-07</td>
-          <td>기타 정보</td>
-        </tr>
-      </table>
-    </div>
+    <div class="Text">전송되었습니다.</div>
   </div>
 </template>
 
@@ -52,18 +12,8 @@ export default { components: { ProcedureBox } };
 </script>
 
 <style scoped>
-.List {
-  margin: 0px 50px;
-  background-color: #e2e2e2;
-}
-.List > table {
-  padding: 10px;
-
-  width: 100%;
-  text-align: left;
-}
-td,
-th {
-  padding: 10px;
+.Text {
+  padding: 2em;
+  text-align: center;
 }
 </style>
